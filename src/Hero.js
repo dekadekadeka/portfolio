e243ef93bed12078acecb5f72df4d98467a0fd7a
github username <dekadekadeka@gmail.com>
2019-09-09 13:07:38 -0400

    first commit

---
+++ src/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderText from './HeaderText'
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <HeaderText/>
+            <h1>Full Stack Web Developer</h1>
+        </div>
+    );
+}
+
+export default Hero;
